37c0f5ef496313f630c71f307be06b772b8be10e
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 00:57:02 -0500

    Add files via upload

---
+++ cas-revision-baril-3q5-Samuel-Bourque/Cargo_Affaires/Baril.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargo_Affaires
+{
+    public class Baril
+    {
+        private int _quantite;
+        private string _nomProduit;
+        public const int CAPACITE = 50;
+
+        public Baril(int qte, string nomProduit)
+        {
+            _quantite = qte;
+            _nomProduit = nomProduit;
+        }
+        #region Proprietes
+        public int Quantite { get { return _quantite; } set { _quantite = value; } }
+        public string NomProduit { get { return _nomProduit; } set { _nomProduit = value; } }
+        #endregion
+
+        public bool estPlein() { return _quantite == CAPACITE; }
+        public bool estVide() { return _quantite == 0; }
+        public bool estCompatible(Baril autreBaril) { return _nomProduit.Equals(autreBaril.NomProduit); }
+        public void transvider(Baril autreBaril)
+        {
+            // verifier si compatible
+            if(this.estCompatible(autreBaril))
+            {
+                int dispo = CAPACITE - _quantite;
+                if(autreBaril.Quantite <= dispo)
+                {
+                    _quantite += autreBaril.Quantite;
+                    autreBaril.Quantite = 0;
+                }
+                else
+                {
+                    _quantite = CAPACITE;
+                    autreBaril.soustraire(dispo);
+                }
+            }
+        }
+        public void soustraire(int qte)
+        {
+            _quantite -= qte;
+            if (_quantite < 0)
+                _quantite = 0;
+        }
+    }
+}
---
+++ cas-revision-baril-3q5-Samuel-Bourque/Cargo_Affaires/Cargo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargo_Affaires
+{
+    public class Cargo
+    {
+        private string _nom;
+        private int _capacite;
+        private List<Baril> _barils;
+        #region Proprietes
+        public string Nom { get { return _nom; } set { _nom = value; } }
+        public int Capacite { get { return _capacite; } set { _capacite = value; } }
+        public List<Baril> Barils { get { return _barils; } set { _barils = value; } }
+        #endregion
+
+        public Cargo(string nom, int capacite)
+        {
+            _nom = nom;
+            _capacite = capacite;
+            _barils = new List<Baril>();
+        }
+        public void ajouter(Baril baril)
+        {
+            // completer le dernier baril compatible
+            // s'il reste toujours une qte dans le baril recu, 
+            // ajouter un nouveau baril avec ce contenu, vider le baril recu
+            Baril trouve = null;
+            foreach (Baril b in _barils)
+                if (b.estCompatible(baril))
+                    trouve = b;
+            if(trouve != null)
+                trouve.transvider(baril);
+            if(!baril.estVide() && !this.estPlein())
+            {
+                Baril nouveau = new Baril(0, baril.NomProduit);
+                _barils.Add(nouveau);
+                nouveau.transvider(baril);
+            }
+        }
+        public int calculerNbUnites(string nomProduit)
+        {
+            int total = 0;
+            foreach (Baril b in _barils)
+                if (b.NomProduit.Equals(nomProduit))
+                    total += b.Quantite;
+            return total;
+        }
+        public bool estPlein()
+        {
+            return _capacite == _barils.Count;
+        }
+        public int denombrerBarils()
+        {
+            return _barils.Count;
+        }
+    }
+}
